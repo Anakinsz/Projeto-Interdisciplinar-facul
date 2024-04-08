44dc18c20c0e5d69208dd222a1263a4c5ebbf700
anakin <anakin>
2024-04-07 22:14:17 -0300

    primeiro passo

---
+++ projeto.py
@@ -0,0 +1,8 @@
+#Projeto Interdisciplinar, apresentar estrutura com menu é possibilidade de escolha da função desejada, utilizar conceitos de sistemas de numeração, conversão entre base aritmética binária com inteiros.
+
+num = int(input("Digite um número inteiro: "))
+print('''Escolha uma função para base de conversão:  
+[1] conversão para Binário
+[2] conversão para Octal
+[3] conversão para hexadecimal''')
+opção = int(input("sua opção: "))
