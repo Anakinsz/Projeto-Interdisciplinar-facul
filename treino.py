104b3ccc43e17c9972c0c7f76307ab341d553f46
Anakin <160639020+Anakinsz@users.noreply.github.com>
2024-05-08 21:02:25 -0300

    Delete treino.py

--- treino.py
+++
@@ -1,9 +0,0 @@
-def conversao_decimal_para_binario(decimal):
-    if decimal == 0:
-        return "0"
-
-    binario = ""
-    while decimal > 0:
-        resto = decimal % 2
-        binario = str(resto) + binario
-        decimal //= 2
